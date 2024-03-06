9c7e7adb97c38532ae1ee325de41c21fb7c37428
Kate <katshe18@gmail.com>
2024-03-06 14:58:31 +1300

    New vue project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+// import Homepage from './components/Homepage/Homepage.vue'
+// import About from './components/About/About.vue'
+</script>
+
+<template>
+
+  <div class="wrapper">
+    <Homepage />
+  </div>
+
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>./components/Navigation/HelloWorld.vue./components/Homepage/HelloWorld.vue
---
+++ src/components/About/About.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>hello vue</div>
+</template>
+
+<style scoped></style>
